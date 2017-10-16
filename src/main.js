756c89e91b2a6ba4ae73e18c023db8d1984dfae9
Christopher Wilson <cmw.wilson88@gmail.com>
2017-10-16 12:21:30 -0400

    set up vue-router and camel-case filter in main.js'

--- src/main.js
+++ src/main.js
@@ -1,7 +1,25 @@
 import Vue from 'vue'
+import VueRouter from 'vue-router'
 import App from './App.vue'
+
+import {routes} from './routes/routes'
+
+// Vue Router setup
+Vue.use(VueRouter)
+
+const router = new VueRouter({
+	routes: routes
+})
+
+//create global filter
+Vue.filter('camel-case', value => {
+	return value.split(' ').map(word => {
+		return word[0].toUpperCase() + words.substr(1)
+	}).join (' ')
+})
 
 new Vue({
   el: '#app',
+  router,
   render: h => h(App)
 })
